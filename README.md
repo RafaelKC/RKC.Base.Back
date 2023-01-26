a2dde24cfd4dfa5333ca13df8c8348e22e7b5758
RafaelKC <rafaelkaua97@gmail.com>
2023-01-26 19:28:54 -0300

    RBB-001 added TestUtilsBase

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to publish this package
+
+- Change version on _package.jon_;
+- Run `npm run prepublish` to build project;
+- Run `npm publish`.
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { IUserCredential, UserCredential } from "./authentication";
 import { IBaseEntity, BaseEntity } from "./entities";
+import { TestUtilsBase } from "./Tests/test-urils-base";
 import { IUser, User } from "./users";
 
 export {
-    IUserCredential, UserCredential,
-    IBaseEntity, BaseEntity,
-    IUser, User
-}
+  IUserCredential,
+  UserCredential,
+  IBaseEntity,
+  BaseEntity,
+  IUser,
+  User,
+  TestUtilsBase,
+};
