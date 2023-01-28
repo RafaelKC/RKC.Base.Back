189b1f5b750b397ecd33488c4e2d67f0ceac047c
RafaelKC <rafaelkaua97@gmail.com>
2023-01-27 21:38:55 -0300

    RBB-003 fixed  and PagedGetListResult export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { IUserCredential, UserCredential } from "./authentication";
 import { IBaseEntity, BaseEntity } from "./entities";
 import { TestUtilsBase } from "./Tests/test-urils-base";
 import { IUser, User } from "./users";
+import PagedGetListInput from "./pagination/paged-get-list-input.dto";
+import { PagedGetListResult } from "./pagination/paged-get-list-result.dto";
 
 export {
   IUserCredential,
@@ -11,4 +13,6 @@ export {
   IUser,
   User,
   TestUtilsBase,
+  PagedGetListInput,
+  PagedGetListResult,
 };
